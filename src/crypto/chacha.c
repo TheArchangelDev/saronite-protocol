56faeed04e1bcd8f51a6e144d9a77ede11328a75
TheArchangelDev <thearchangelhs@outlook.com>
2018-10-31 22:14:39 +0200

    Update src/crypto/chacha.c

--- src/crypto/chacha.c
+++ src/crypto/chacha.c
@@ -171,11 +171,6 @@ static void chacha8(unsigned rounds, const void* data, size_t length, const uint
   }
 }
 
-void chacha8(const void* data, size_t length, const uint8_t* key, const uint8_t* iv, char* cipher)
-{
-  chacha(8, data, length, key, iv, cipher);
-}
-
 void chacha20(const void* data, size_t length, const uint8_t* key, const uint8_t* iv, char* cipher)
 {
   chacha(20, data, length, key, iv, cipher);
